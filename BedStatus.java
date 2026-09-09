4d5509b49d6c21d281459cf43f9a72596b32c1a3
regomoditswemoc-crypto <regomoditswemoc@gmail.com>
2026-09-09 15:37:08 +0200

    Add files via upload
    Uploaded my BedStatus file

---
+++ BedStatus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hospital.enums;
+
+/**
+ *
+ * 
+ */
+// Shows if a bed is free or taken 
+public enum BedStatus {
+    AVAILABLE,
+    OCCUPIED
+    
+}
